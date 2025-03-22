605729b544b274154e2987749a0488082a4f5309
Black Shadow <mithabozdar2@gmail.com>
2025-03-23 01:22:10 +0500

    Complete template, Revision No.3

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+
+
+  // Disable rules that may conflict with prettier
+  eslintConfigPrettier,
 ]);
